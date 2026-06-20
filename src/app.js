4d20591e63738bf9c5459c92c6c7d4911e3f81a0
rrrgsrssingh <sauravlkr69@gmail.com>
2026-06-20 16:03:00 +0530

    Playing with route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const app=express();
-app.use((req,res)=>{
+app.use("/saurav",(req,res)=>{
     res.send("Hello Saurav I am calling from the server")
 });
-port=4000;
+app.get("/user",(req,res)=>{
+  res.send({firstName:"Saurav",lastName:"Kumar" ,age:"28", Address:"At Nadiyawan dist-Lakhisarai State-Bihar"});
+});
+port=7000;
 app.listen(port,()=>{
     console.log(" I am Up Saurav Kumar")
 })
