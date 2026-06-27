3be124fc20cd39a64a651ced8113300c6dee3a3a
rrrgsrssingh <sauravlkr69@gmail.com>
2026-06-27 21:38:00 +0530

    validation on database

--- src/app.js
+++ src/app.js
@@ -1,52 +1,28 @@
 const express=require("express");
 const app=express();
-// app.use("/saurav",(req,res)=>{
-//     res.send("Hello Saurav I am calling from the server")
-// });
-const {authAdmin,authUser}= require("./middleware/auth")
-app.use("/admin",authAdmin);
-app.use("/user",authAdmin);
-
-app.get("/admin/getAlldata",(req,res)=>{
-    res.send("Data Sent Back");
-});
-app.get("/admin/deleteData",(req,res)=>{
-    res.send("data Deleted");
-})
-app.get("/user/loggedin",(req,res)=>{
-    res.send("Login SuccessFull");
-})
-app.get("/user/loggedout",(req,res)=>{
-    res.send("Logout Successfully");
-})
-// Error Handling
-app.get("/userData",(req,res)=>{
+const dns=require("dns");
+dns.setServers(["1.1.1.1","8.8.8.8"]);
+const connectDB=require("./config/database");
+const User=require("./model/user");
+app.use(express.json());
+app.post("/signup",async (req,res)=>{
+    const user=new User(req.body);
     try{
-        throw new error("xyshdhd");
-        
-        res.send("Hello");
-    }
+    await user.save();
+    res.send("Data has been succesfully saved");}
     catch(err){
-        res.status(500).send("Something Went Wrong");
+        res.status(400).send("Bad Request");
     }
 })
-app.use("/",(err,req,res,nex)=>{
-    if(err){
-        res.send(500).send("Calling from the wild error Handling");
-    }
-});
-// app.use("/saurav",(req,res,next)=>{
-//     //res.send("This is the first route handler");
-//     next();},
-//     (req,res)=>{
-//         res.send("Second Route Handler");
-//     }
-// )
-// app.get("/user/:userId/:password",(req,res)=>{
-//     console.log(req.params);
-//   res.send({firstName:"Saurav",lastName:"Kumar" ,age:"28", Address:"At Nadiyawan dist-Lakhisarai State-Bihar"});
-// });
+connectDB().then(()=>{
+    console.log("DataBase Connection Established");
+    
 port=7000;
 app.listen(port,()=>{
     console.log(" I am Up Saurav Kumar")
+});
 })
+.catch((err)=>{
+    console.log("DataBase Connection has lost",err.message)
+});
+
---
+++ src/readme.md
@@ -0,0 +1,46 @@
+// app.use("/saurav",(req,res)=>{
+//     res.send("Hello Saurav I am calling from the server")
+// });
+const {authAdmin,authUser}= require("./middleware/auth")
+app.use("/admin",authAdmin);
+app.use("/user",authAdmin);
+
+app.get("/admin/getAlldata",(req,res)=>{
+    res.send("Data Sent Back");
+});
+app.get("/admin/deleteData",(req,res)=>{
+    res.send("data Deleted");
+})
+app.get("/user/loggedin",(req,res)=>{
+    res.send("Login SuccessFull");
+})
+app.get("/user/loggedout",(req,res)=>{
+    res.send("Logout Successfully");
+})
+// Error Handling
+app.get("/userData",(req,res)=>{
+    try{
+        throw new error("xyshdhd");
+        
+        res.send("Hello");
+    }
+    catch(err){
+        res.status(500).send("Something Went Wrong");
+    }
+})
+app.use("/",(err,req,res,nex)=>{
+    if(err){
+        res.send(500).send("Calling from the wild error Handling");
+    }
+});
+// app.use("/saurav",(req,res,next)=>{
+//     //res.send("This is the first route handler");
+//     next();},
+//     (req,res)=>{
+//         res.send("Second Route Handler");
+//     }
+// )
+// app.get("/user/:userId/:password",(req,res)=>{
+//     console.log(req.params);
+//   res.send({firstName:"Saurav",lastName:"Kumar" ,age:"28", Address:"At Nadiyawan dist-Lakhisarai State-Bihar"});
+// });
