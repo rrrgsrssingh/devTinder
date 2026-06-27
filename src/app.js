7c5123a3763a4c4853873202ad6d4d5814f73eb0
rrrgsrssingh <sauravlkr69@gmail.com>
2026-06-27 21:38:31 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -11,7 +11,64 @@ app.post("/signup",async (req,res)=>{
     await user.save();
     res.send("Data has been succesfully saved");}
     catch(err){
-        res.status(400).send("Bad Request");
+        if (err.code === 11000) {
+            return res.status(400).send("Registration failed: That email address is already in use.");
+        }
+
+        // Handle other validation errors (like short names or missing passwords)
+        res.status(400).send("Validation error: " + err.message);
+    }
+});
+app.get("/feed", async (req,res)=>{
+    try{
+     const users=await User.find({});
+     res.send(users);
+    }
+    catch(err){
+        res.status(404).send("User Not Found");
+    }
+});
+app.get("/user",async (req,res)=>{
+    const userEmail=req.body.emailid;
+    try{
+     const user= await User.findOne({emailid:userEmail});
+     if(user.length===0){
+        res.send("User Not found")
+     }
+     else{
+        res.send(user);
+     }
+    }
+    catch(err){
+        res.status(404).send("User Not Found");
+    }
+
+});
+app.delete("/user", async (req,res)=>{
+    const userID=req.body.userID
+    try{
+        const user=await User.findByIdAndDelete(userID);
+        res.send("User Deleted Succesfully");
+    }
+    catch(err){
+        res.status(400).send("Something Went Wrong")
+    }
+})
+app.patch("/user", async (req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+
+    
+    try{
+        await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument:"after",
+            runValidators:true
+        });
+        res.send("User Updated Successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+
     }
 })
 connectDB().then(()=>{
--- src/model/user.js
+++ src/model/user.js
@@ -1,22 +1,53 @@
 const mongoose=require("mongoose");
 const userSchema=new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        unique: true,
+     minlength:4,
+     maxlength:50
     },
     lastName: {
-        type:String
+        type:String,
     },
     emailid:{
-        type:String
+        type:String,
+        required:true,
+        index:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
     },
     password:{
-        type:String
-    },
+        type:String,
+        required:true,
+        },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        max:65
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!['male','female','others'].includes(value)){
+                throw new Error("Gender Data is not valid please take male female or others");
+            }
+
+        },
+    },
+    about:{
+        type:"String",
+        default:"This is the About page you can write anything About you.",
+    },
+    skills:{
+        type:["String"],
+    },
+    photourl:{
+        type:"String",
+        default:"https://img.magnific.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?semt=ais_hybrid&w=740&q=80",
     }
+},{
+    timestamps:true
 })
 module.exports=mongoose.model("User",userSchema);
